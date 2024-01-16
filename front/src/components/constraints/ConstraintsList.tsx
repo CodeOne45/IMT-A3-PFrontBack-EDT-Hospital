5bc69d846b06cc5f3c201fc9b340d925a1480079
Pierre Leroyer <pierre.leroyer69@gmail.com>
2024-01-16 14:29:26 +0100

    Refactor ConstraintsList component to filter and group constraints by type

--- front/src/components/constraints/ConstraintsList.tsx
+++ front/src/components/constraints/ConstraintsList.tsx
@@ -20,20 +20,20 @@ export const ConstraintsList = ({
   let constraintsByType: { [key: string]: Constraint[] } = {};
 
   // regrouper les contraintes par type
-  constraints.filter((c) => !removedConstraints.includes(c.id!));
-
-  constraints.forEach((constraint) => {
-    if (!constraintsByType[constraint.type!]) {
-      constraintsByType[constraint.type!] = [];
-    }
-    constraintsByType[constraint.type!].push(constraint);
-  });
+  constraints
+    .filter((c) => !removedConstraints.includes(c.id))
+    .forEach((constraint) => {
+      if (!constraintsByType[constraint.type!]) {
+        constraintsByType[constraint.type!] = [];
+      }
+      constraintsByType[constraint.type!].push(constraint);
+    });
 
   const removeConstraint = (cons: Constraint) => {
     console.log("remove constraint", cons.id);
     removedConstraints.push(cons.id);
-    // setRemovedConstraints([...removedConstraints]);
-    onUpdateConstraints(constraints.filter((c) => c.id !== cons.id));
+    setRemovedConstraints([...removedConstraints]);
+    // onUpdateConstraints(constraints.filter((c) => c.id !== cons.id));
   };
 
   // afficher les contraintes de chaque type
@@ -44,16 +44,16 @@ export const ConstraintsList = ({
           {addedConstraints.length > 0 && (
             <Box sx={{ background: green[50] }}>
               <h2>new Constraints</h2>
+              {addedConstraints.map((constraint) => (
+                <ConstraintsListItem
+                  key={constraint.id}
+                  constraint={constraints.find((c) => c.id === constraint.id)!}
+                  onRemove={() => {}}
+                  canRemove={false}
+                />
+              ))}
             </Box>
           )}
-          {addedConstraints.map((constraint) => (
-            <ConstraintsListItem
-              key={constraint}
-              constraint={constraints.find((c) => c.id === constraint)!}
-              onRemove={() => {}}
-              canRemove={false}
-            />
-          ))}
           {removedConstraints.length > 0 && (
             <Box sx={{ background: red[50] }}>
               <h2>Removed Constriants</h2>
@@ -61,8 +61,14 @@ export const ConstraintsList = ({
                 <ConstraintsListItem
                   key={"removed" + constraintId}
                   constraint={constraints.find((c) => c.id === constraintId)!}
-                  onRemove={() => {}}
-                  canRemove={false}
+                  onRemove={() => {
+                    removedConstraints.splice(
+                      removedConstraints.indexOf(constraintId),
+                      1
+                    );
+                    setRemovedConstraints([...removedConstraints]);
+                  }}
+                  canRemove={true}
                 />
               ))}
             </Box>
