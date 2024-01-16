8f9c5d6dbecea24735d39fc0cc5af302822e0798
Mael29000 <maelsuard@orange.fr>
2024-01-16 13:43:38 +0100

    fix useState bug

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,12 @@
+import { useEffect } from "react";
 import { useUpdatedConstraints } from "./contexts/UpdatedConstraintsContext";
 import Home from "./pages/Home";
 
 function App() {
   const { init } = useUpdatedConstraints();
-  init();
+  useEffect(() => {
+    init();
+  }, []);
   return <Home />;
 }
 
